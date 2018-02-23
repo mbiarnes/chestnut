25a53e7b9a0c9bc7996ee3f4babd7bee3ae6aa7d
Michael Biarnés Kiefer <mbiarnes@redhat.com>
2018-02-23 17:11:26 +0100

    Create michael.groovy

---
+++ michael.groovy
@@ -0,0 +1,8 @@
+def date = new Date().format( 'yyyyMMdd-hhMMss' )
+    def kieVersionPre = "7.7.0."
+    def appformerVersionPre = "2.4.0."
+    def erraiVersionNewPre = "4.2.0."
+    def sourceProductTag = ""
+    def targetProductBuild = ""
+
+    return [kieVersion: kieVersionPre + date, appformerVersion: appformerVersionPre + date, erraiVersionNew:erraiVersionNewPre +date,  cutOffDate: date, reportDate: date,  sourceProductTag: sourceProductTag, targetProductBuild: targetProductBuild]  
