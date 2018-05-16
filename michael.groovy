7610a7067b64b0b58b36f110a8557494744d348e
Michael Biarnés Kiefer <mbiarnes@redhat.com>
2018-05-16 17:30:36 +0200

    Update michael.groovy

--- michael.groovy
+++ michael.groovy
@@ -7,12 +7,17 @@ def mvnToolEnv="APACHE_MAVEN_3_3_9"
 def mvnVersion="apache-maven-3.3.9"
 def mvnHome="${mvnToolEnv}_HOME"
 def mvnOpts="-Xms2g -Xmx3g"
-def kieMainBranch="master"
-def organization="kiegroup"
-
+def mainBranch="master"
+def organization="mbiarnes"
+def mainRepo="chestnut"
+def tag="kunnigunde"
+def baseBranch="master"    
+def releaseBranch="r8.8.0.Final"
+def uberfireVersion="2.4.1"
+def releaseVersion="8.8.0.Final"
 
 def createReleaseBranches="""
-sh \$WORKSPACE/droolsjbpm-build-bootstrap/script/release/kie-createReleaseBranches.sh
+sh \$WORKSPACE/chestnut/releasing-KIE--push-release-branches.sh
 """
 
 // **************************************************************************
@@ -20,17 +25,23 @@ sh \$WORKSPACE/droolsjbpm-build-bootstrap/script/release/kie-createReleaseBranch
 job("mbiarnes-test-by-dsl") {
 
     description("test test test")
-
+    parameters{
+        stringParam("tag", "${tag}", "This will be usually set automatically by the parent trigger job. ")
+        stringParam("uberfireVersion", "${uberfireVersion}", "This will be usually set automatically by the parent trigger job. ")
+        stringParam("releaseVersion", "${releaseVersion}", "This will be usually set automatically by the parent trigger job. ")
+        stringParam("releaseBranch", "${releaseBranch}", "This will be usually set automatically by the parent trigger job. ")
+        stringParam("baseBranch", "${baseBranch}", "This will be usually set automatically by the parent trigger job. ")
+   }
     scm {
         git {
             remote {
-                github("${organization}/droolsjbpm-build-bootstrap")
+                github("${organization}/${mainRepo}")
             }
             branch ("${kieMainBranch}")
             extensions {
-                relativeTargetDirectory("droolsjbpm-build-bootstrap")
+                relativeTargetDirectory("${mainRepo}")
                 localBranch {
-                   localBranch("${kieMainBranch}")
+                   localBranch("${mainBranch}")
                 } 
             }
         }
