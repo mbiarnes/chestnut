f8c68281af8daba71797db67bdb4b801e2bc054b
Michael Biarnes Kiefer <mbiarnes@redhat.com>
2018-05-17 15:26:33 +0200

    removed everything

--- yyy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-# clone rest of the repos
-./droolsjbpm-build-bootstrap/script/git-clone-others.sh --branch $baseBranch --depth 10
-
-# git checkout to $baseBranch
-./droolsjbpm-build-bootstrap/script/git-all.sh checkout $baseBranch
