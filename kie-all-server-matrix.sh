7fe8f8f3588fb7e8b071b48230fe8824c27a0e90
Michael Biarnes Kiefer <mbiarnes@redhat.com>
2018-05-18 17:34:58 +0200

    third scripts

--- kie-all-server-matrix.sh
+++ kie-all-server-matrix.sh
@@ -2,8 +2,6 @@
 
 echo "kieVersion:" $kieVersion
 echo "target" : $target
-echo "WORKSPACE :" $WORKSPACE
-echo "deployDir :" $deployDir
 
 if [ "$target" == "community" ]; then
    stagingRep=kie-group
@@ -14,13 +12,9 @@ elif [ "$target" == "productized" ]; then
    # wget the tar.gz sources
    wget -q https://repository.jboss.org/nexus/content/groups/$stagingRep/org/drools/droolsjbpm-integration/$kieVersion/droolsjbpm-integration-$kieVersion-project-sources.tar.gz -O sources.tar.gz
 elif [ "$target" == "daily" ]; then
-   # unpack zip to QA Nexus
-   cd $deployDir
-   zip -r kiegroup .
-   curl --upload-file kiegroup.zip -u $kieUnpack -v http://bxms-qe.rhev-ci-vms.eng.rdu2.redhat.com:8081/nexus/service/local/repositories/kieAllBuild/content-compressed
-   cd ..
+   # wget the tar.gz sources
+   wget -q http://bxms-qe.rhev-ci-vms.eng.rdu2.redhat.com:8081/nexus/content/repositories/kieAllBuild/org/drools/droolsjbpm-integration/$kieVersion/droolsjbpm-integration-$kieVersion-project-sources.tar.gz -O sources.tar.gz
 fi
-
 
 tar xzf sources.tar.gz
 mv droolsjbpm-integration-$kieVersion/* .
---
+++ kie-jbpm-test-coverage-matrix.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+echo "kieVersion:" $kieVersion
+echo "target" : $target
+
+if [ "$target" == "community" ]; then
+   stagingRep=kie-group
+   # wget the tar.gz sources
+   wget -q https://repository.jboss.org/nexus/content/groups/$stagingRep/org/jbpm/jbpm/$kieVersion/jbpm-$kieVersion-project-sources.tar.gz -O sources.tar.gz
+elif [ "$target" == "productized" ]; then
+   stagingRep=kie-internal-group
+   # wget the tar.gz sources
+   wget -q https://repository.jboss.org/nexus/content/groups/$stagingRep/org/jbpm/jbpm/$kieVersion/jbpm-$kieVersion-project-sources.tar.gz -O sources.tar.gz
+elif [ "$target" == "daily" ]; then
+   # wget the tar.gz sources
+   wget -q http://bxms-qe.rhev-ci-vms.eng.rdu2.redhat.com:8081/nexus/content/repositories/kieAllBuild/org/jbpm/jbpm/$kieVersion/jbpm-$kieVersion-project-sources.tar.gz -O sources.tar.gz
+fi
+
+tar xzf sources.tar.gz
+mv jbpm-$kieVersion/* .
+rmdir jbpm-$kieVersion
